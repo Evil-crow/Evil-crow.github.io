87f4fbd575e54926f28c64976e609730e8044f43
Evil-crow <Evilcrow486@gmail.com>
2018-10-18 15:08:17 +0800

    Site updated: 2018-10-18 15:08:17

--- js/gitment.browser.js
+++ js/gitment.browser.js
@@ -3415,7 +3415,7 @@ var Gitment = function () {
       }, options);
 
       this.state.user.isLoggingIn = true;
-      _utils.http.post('https://39.107.122.183:3000', {
+      _utils.http.post('https://auth.baixiaotu.cc', {
         code: code,
         client_id: client_id,
         client_secret: client_secret
